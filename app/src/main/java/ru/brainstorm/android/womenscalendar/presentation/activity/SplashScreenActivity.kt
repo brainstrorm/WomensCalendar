5ec3edd4472a9fb4b3e1af2012f646019326a169
skyscrraper <hungrydog757@gmail.com>
2019-10-14 20:42:13 +0300

    done

--- app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/activity/SplashScreenActivity.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/activity/SplashScreenActivity.kt
@@ -8,7 +8,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_splash_screen) //smtg
     }
 
 }
