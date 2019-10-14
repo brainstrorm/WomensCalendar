127bbdf9150fa10f34b39fc0e7902aeca171e702
Aroize <gipermonk@bk.ru>
2019-10-14 17:49:26 +0300

    initialized SplashScreen with activity

---
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/activity/SplashScreenActivity.kt
@@ -0,0 +1,14 @@
+package ru.brainstorm.android.womenscalendar.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.brainstorm.android.womenscalendar.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+}
