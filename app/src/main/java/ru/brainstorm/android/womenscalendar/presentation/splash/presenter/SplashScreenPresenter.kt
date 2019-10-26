0517b6083edb538c7bc13d5d24580775063ecc20
Aroize <gipermonk@bk.ru>
2019-10-26 13:03:19 +0300

    check of first activation - added getAll() from db

--- app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/splash/presenter/SplashScreenPresenter.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/splash/presenter/SplashScreenPresenter.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.*
 import moxy.InjectViewState
 import moxy.MvpPresenter
 import ru.brainstorm.android.womenscalendar.data.User
+import ru.brainstorm.android.womenscalendar.data.database.dao.CycleDao
 import ru.brainstorm.android.womenscalendar.domain.repository.ReadUserInfoRepositoryImpl
 import ru.brainstorm.android.womenscalendar.presentation.splash.view.SplashScreenView
 import javax.inject.Inject
@@ -14,14 +15,18 @@ import javax.inject.Inject
  */
 @InjectViewState
 class SplashScreenPresenter
-    @Inject constructor(private val readUserInfoRepository: ReadUserInfoRepositoryImpl)
+    @Inject constructor(
+        private val readUserInfoRepository: ReadUserInfoRepositoryImpl,
+        private val cycleDao: CycleDao)
         : MvpPresenter<SplashScreenView>() {
 
     fun checkFirstLaunch() {
         GlobalScope.launch(Dispatchers.Main) {
             delay(1_000)
-            readUserInfoRepository.readUserInfoAsync().join()
-            if (User.isInitialized()) {
+            val readJob = readUserInfoRepository.readUserInfoAsync()
+            val cycles = cycleDao.getAll()
+            readJob.join()
+            if (User.isInitialized() && cycles.isNotEmpty()) {
                 viewState.goToCalendar()
             } else {
                 //DEBUG VERSION
