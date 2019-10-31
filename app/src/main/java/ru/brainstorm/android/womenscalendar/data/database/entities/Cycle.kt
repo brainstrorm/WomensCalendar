b446ee2ad4fe5c425768160aff74e5e0e471a00f
JelliedFish <hungrydog757@gmail.com>
2019-11-01 00:30:24 +0300

    Predictor v.1.2 / fix buttons in

--- app/src/main/java/ru/brainstorm/android/womenscalendar/data/database/entities/Cycle.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/data/database/entities/Cycle.kt
@@ -8,15 +8,23 @@ import androidx.room.PrimaryKey
  * @author Ilia Ilmenskii created on 26.10.2019
  */
 @Entity
-class Cycle {
+class Cycle(startOfCycle: String = "1.10.2019",ovulation: String = "15.10.2019",lengthOfCycle: Int = 24,lengthOfMenstruation: Int = 5)
+{
+
+
+
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
 
     lateinit var startOfCycle: String
 
+    lateinit var ovulation: String
+
     var lengthOfCycle: Int = 0
+
 
     var lengthOfMenstruation: Int = 0
 
     var predicted: Boolean = false
+
 }
---
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/domain/predictor/PredictorImpl.kt
@@ -0,0 +1,65 @@
+package ru.brainstorm.android.womenscalendar.domain.predictor
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import ru.brainstorm.android.womenscalendar.data.database.dao.CycleDao
+import ru.brainstorm.android.womenscalendar.data.database.entities.Cycle
+import java.time.LocalDate
+import javax.inject.Inject
+
+/**
+ * @project WomensCalendar
+ * @author Ilia Ilmenskii created on 27.10.2019
+ */
+class PredictorImpl
+    @Inject constructor(val cycleDao: CycleDao)
+        : Predictor {
+    override suspend fun predict(count: Int): Job = GlobalScope.launch(Dispatchers.IO) {
+
+
+        val set_inject: List<Cycle> = cycleDao.getAll()
+        val setLengthofmenstruation: MutableList<Int> = mutableListOf()
+        val setLengthofcycle: MutableList<Int> = mutableListOf()
+        val setOvulations: MutableList<String> = mutableListOf()
+
+
+        if(set_inject.size == 1) {
+
+                if((set_inject[0].lengthOfCycle < 21)&&(set_inject[0].lengthOfCycle > 35)){
+                    set_inject[0].lengthOfCycle = 24
+                }
+
+                if((set_inject[0].lengthOfMenstruation < 2)&&(set_inject[0].lengthOfMenstruation > 8)){
+                    set_inject[0].lengthOfMenstruation = 5
+                }
+
+                  set_inject[0].ovulation = LocalDate.parse(set_inject.last().startOfCycle).plusDays(set_inject[0].lengthOfCycle-14.toLong()).toString()
+            }
+
+        for (cycle in set_inject) {
+            setLengthofmenstruation.add(cycle.lengthOfMenstruation)
+            setLengthofcycle.add(cycle.lengthOfCycle)
+            setOvulations.add(cycle.ovulation)
+        }
+
+        val avgSetLengthofmenstruation = setLengthofcycle.average().toLong()
+        val avgSetLengthofcycle = setLengthofmenstruation.average().toLong()
+
+       var set_update =  set_inject.toMutableList()
+
+        for(i in 1..count){
+
+            var firstDayOfNewCycle: String = LocalDate.parse(set_update.last().startOfCycle).plusDays(avgSetLengthofcycle).toString()
+
+            val ovulation = LocalDate.parse(firstDayOfNewCycle).plusDays(avgSetLengthofcycle-14.toLong()).toString()
+
+            val newCycle = Cycle(firstDayOfNewCycle,ovulation,avgSetLengthofcycle.toInt(),avgSetLengthofmenstruation.toInt())
+
+            set_update.add(newCycle)
+            cycleDao.insert(newCycle)
+        }
+    }
+}
+
