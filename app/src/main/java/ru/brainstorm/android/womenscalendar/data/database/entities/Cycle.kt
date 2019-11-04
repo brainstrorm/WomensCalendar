baa0db6a5d45c65f5f5f11ab4f5d9327c7a55352
Aroize <gipermonk@bk.ru>
2019-11-04 15:21:56 +0300

    fix fatal db error

--- app/src/main/java/ru/brainstorm/android/womenscalendar/data/database/entities/Cycle.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/data/database/entities/Cycle.kt
@@ -8,9 +8,7 @@ import androidx.room.PrimaryKey
  * @author Ilia Ilmenskii created on 26.10.2019
  */
 @Entity
-class Cycle(startOfCycle: String = "1.10.2019",ovulation: String = "15.10.2019",lengthOfCycle: Int = 24,lengthOfMenstruation: Int = 5) {
-
-
+class Cycle {
 
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
