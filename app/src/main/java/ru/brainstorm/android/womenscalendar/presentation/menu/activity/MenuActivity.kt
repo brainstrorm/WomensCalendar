ce0720922f96a730b57f68d9d8be2f5f018be7dc
JelliedFish <hungrydog757@gmail.com>
2019-11-17 01:31:09 +0300

    fix header in menu activity && create buttons / colors in menu activity

--- app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/activity/MenuActivity.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/activity/MenuActivity.kt
@@ -37,12 +37,16 @@ class MenuActivity : MvpAppCompatActivity(), View.OnClickListener, MenuView {
     private lateinit var btnMonthOrYear : ImageView
     private lateinit var btnToday : ImageButton
     private lateinit var btnCalendar : ImageButton
-    private lateinit var btnInfo : ImageButton
+    private lateinit var btnInfo : ImageView
     private lateinit var btnMore : ImageButton
     private lateinit var layoutLeft : ConstraintLayout
     private lateinit var layoutRight : ConstraintLayout
     private lateinit var btnPlusNote : ImageButton
     private lateinit var btnNewDates : ImageButton
+    private lateinit var txtvwCalendar : TextView
+    private lateinit var txtvwToday : TextView
+    private lateinit var txtvwNotes : TextView
+    private lateinit var txtvwMore : TextView
     private lateinit var txtvwNewDates : TextView
     private lateinit var layoutForNotes : FrameLayout
     private lateinit var btnBack : ImageView
@@ -68,7 +72,7 @@ class MenuActivity : MvpAppCompatActivity(), View.OnClickListener, MenuView {
         btnStatistics = findViewById<ImageView>(R.id.btn_statistics).apply { setOnClickListener(this@MenuActivity) }
         btnToday = findViewById<ImageButton>(R.id.today).apply{setOnClickListener(this@MenuActivity)}
         btnCalendar = findViewById<ImageButton>(R.id.calendar).apply{setOnClickListener(this@MenuActivity)}
-        btnInfo = findViewById<ImageButton>(R.id.info).apply{setOnClickListener(this@MenuActivity)}
+        btnInfo = findViewById<ImageView>(R.id.info).apply{setOnClickListener(this@MenuActivity)}
         btnMore = findViewById<ImageButton>(R.id.more_text).apply{setOnClickListener(this@MenuActivity)}
         btnPlusNote = findViewById<ImageButton>(R.id.btn_plus_menu).apply { setOnClickListener(this@MenuActivity) }
         btnNewDates = findViewById<ImageButton>(R.id.btn_new_date_menu).apply { setOnClickListener(this@MenuActivity) }
@@ -76,6 +80,10 @@ class MenuActivity : MvpAppCompatActivity(), View.OnClickListener, MenuView {
         layoutForNotes = findViewById<FrameLayout>(R.id.for_notes).apply { setOnClickListener(this@MenuActivity) }
         btnBack = findViewById<ImageView>(R.id.btn_back).apply { setOnClickListener(this@MenuActivity) }
         btnDone = findViewById<TextView>(R.id.done).apply { setOnClickListener(this@MenuActivity) }
+        txtvwCalendar = findViewById<TextView>(R.id.calendar_text)
+        txtvwToday = findViewById<TextView>(R.id.today_text)
+        txtvwNotes = findViewById<TextView>(R.id.info_text)
+        txtvwMore = findViewById<TextView>(R.id.more)
 
         App.appComponent.inject(this)
         menuPresenter.setFragment(supportFragmentManager, "today")
@@ -92,21 +100,60 @@ class MenuActivity : MvpAppCompatActivity(), View.OnClickListener, MenuView {
     }
 
     override fun setPart(part: String) {
+
+        btnToday?.setBackgroundResource(R.drawable.ic_btn_today_menu_grey)
+        btnToday?.layoutParams.width = 21
+        btnToday?.layoutParams.height = 21
+        txtvwToday.setTextColor(resources.getColor(R.color.colorGrey))
+
+        btnCalendar?.setBackgroundResource(R.drawable.ic_btn_calendar_menu_grey)
+        btnCalendar?.layoutParams.width = 21
+        btnCalendar?.layoutParams.height = 21
+        txtvwCalendar.setTextColor(resources.getColor(R.color.colorGrey))
+
+        btnInfo?.setBackgroundResource(R.drawable.ic_btn_notes_for_menu_grey)
+        btnInfo?.layoutParams.width = 18
+        btnInfo?.layoutParams.height = 18
+        txtvwNotes.setTextColor(resources.getColor(R.color.colorGrey))
+
+        btnMore?.setBackgroundResource(R.drawable.ic_btn_more_menu_grey)
+        btnMore?.layoutParams.width = 18
+        btnMore?.layoutParams.height = 18
+        txtvwMore.setTextColor(resources.getColor(R.color.colorGrey))
+
         when(part){
             "today" ->{
                 setVisibility(View.GONE, View.GONE, View.GONE,
                     View.VISIBLE, View.VISIBLE, View.VISIBLE, View.VISIBLE,
                     View.GONE, View.GONE)
+                btnToday?.setBackgroundResource(R.drawable.ic_btn_today_menu_blue)
+                btnToday?.layoutParams.width = 21
+                btnToday?.layoutParams.height = 21
+                txtvwToday.setTextColor(resources.getColor(R.color.colorBlueNotesRedactor))
+
+
             }
             "calendar" ->{
                 setVisibility(View.VISIBLE, View.VISIBLE, View.VISIBLE,
                     View.VISIBLE, View.VISIBLE, View.VISIBLE, View.VISIBLE,
                     View.GONE, View.GONE)
+                btnCalendar?.setBackgroundResource(R.drawable.ic_btn_calendar_menu_blue)
+                btnCalendar?.layoutParams.width = 21
+                btnCalendar?.layoutParams.height = 21
+                txtvwCalendar.setTextColor(resources.getColor(R.color.colorBlueNotesRedactor))
+
+
             }
             "notes" ->{
                 setVisibility(View.VISIBLE, View.GONE, View.GONE,
                     View.GONE, View.VISIBLE, View.VISIBLE, View.VISIBLE,
                     View.GONE, View.GONE)
+                btnInfo?.setBackgroundResource(R.drawable.ic_btn_notes_for_menu_blue)
+                btnInfo?.layoutParams.width = 18
+                btnInfo?.layoutParams.height = 18
+                txtvwNotes.setTextColor(resources.getColor(R.color.colorBlueNotesRedactor))
+
+
             }
             "note_redactor" ->{
                 setVisibility(View.VISIBLE, View.GONE, View.GONE,
@@ -117,6 +164,12 @@ class MenuActivity : MvpAppCompatActivity(), View.OnClickListener, MenuView {
                 setVisibility(View.VISIBLE, View.GONE, View.GONE,
                     View.GONE, View.VISIBLE, View.VISIBLE, View.VISIBLE,
                     View.GONE, View.GONE)
+                btnMore?.setBackgroundResource(R.drawable.ic_btn_more_menu_blue)
+                btnMore?.layoutParams.width = 18
+                btnMore?.layoutParams.height = 18
+                txtvwMore.setTextColor(resources.getColor(R.color.colorBlueNotesRedactor))
+
+
             }
         }
     }
@@ -128,15 +181,19 @@ class MenuActivity : MvpAppCompatActivity(), View.OnClickListener, MenuView {
                 goToStatistic()
             }
             R.id.today -> {
+
                menuPresenter.setFragment(supportFragmentManager, "today")
             }
             R.id.calendar -> {
+
                 menuPresenter.setFragment(supportFragmentManager, "calendar")
             }
             R.id.info -> {
+
                 menuPresenter.setFragment(supportFragmentManager, "notes")
             }
             R.id.more_text -> {
+
                 //TODO
             }
             R.id.btn_plus_menu, R.id.for_notes -> {
