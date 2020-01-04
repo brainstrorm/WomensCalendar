21d212cefb9f07ba313518845aa32bf35d0f8d0d
BigBey <bey.sviatoslav@yandex.ru>
2020-01-05 02:41:39 +0300

    initial commit

--- app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/fragment/ChangeLanguageFragment.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/fragment/ChangeLanguageFragment.kt
@@ -1,14 +1,16 @@
 package ru.brainstorm.android.womenscalendar.presentation.menu.fragment
 
 import android.content.Context
-import android.net.Uri
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import ru.brainstorm.android.womenscalendar.R
+import java.util.*
+
 
 class ChangeLanguageFragment : Fragment() {
 
@@ -22,6 +24,13 @@ class ChangeLanguageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val locale = Locale("en")
+        Locale.setDefault(locale)
+        val configuration = Configuration()
+        configuration.locale = locale
+        context!!.getResources().updateConfiguration(configuration, null)
+        var txtvwToday =  activity!!.findViewById<TextView>(R.id.today_text)
+        txtvwToday.setText(R.string.today)
         return inflater.inflate(R.layout.fragment_change_language, container, false)
     }
 
