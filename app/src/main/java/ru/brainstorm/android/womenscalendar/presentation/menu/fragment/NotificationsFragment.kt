512986245de47123fd26b501359ff956cdbf5ce3
BigBey <bey.sviatoslav@yandex.ru>
2020-01-06 23:19:51 +0300

    add NotificationsFragment

---
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/fragment/NotificationsFragment.kt
@@ -0,0 +1,35 @@
+package ru.brainstorm.android.womenscalendar.presentation.menu.fragment
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import ru.brainstorm.android.womenscalendar.R
+
+class NotificationsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
