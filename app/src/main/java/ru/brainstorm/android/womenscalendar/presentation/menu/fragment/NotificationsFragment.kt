a8ddb86c1a3a1f345332da037a8669b9ea63f72d
JelliedFish <hungrydog757@gmail.com>
2020-01-07 19:49:15 +0300

    some problems with notification ... fix with Slava

--- app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/fragment/NotificationsFragment.kt
+++ app/src/main/java/ru/brainstorm/android/womenscalendar/presentation/menu/fragment/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package ru.brainstorm.android.womenscalendar.presentation.menu.fragment
 
+import android.app.Notification
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -9,15 +10,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Switch
 
 import ru.brainstorm.android.womenscalendar.R
 import ru.brainstorm.android.womenscalendar.presentation.menu.activity.MenuActivity
+import android.content.Context.NOTIFICATION_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import android.app.NotificationManager
+import androidx.media.app.NotificationCompat
+import android.content.Context.NOTIFICATION_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+
+
+
 
 class NotificationsFragment : AbstractMenuFragment() {
 
     private lateinit var notificationsButton : ImageView
     private lateinit var notificationStartMenstruationButton : ImageButton
     private lateinit var notificationEndMenstruationButton : ImageButton
+    private lateinit var switchStartMenstruationButton : Switch
+    private lateinit var switchEndMenstruationButton : Switch
+    private lateinit var switchOpenFetilnostButton : Switch
+    private lateinit var switchOvulationButton : Switch
+    private lateinit var switchCloseFetilnostButton : Switch
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +50,43 @@ class NotificationsFragment : AbstractMenuFragment() {
         notificationsButton = activity!!.findViewById<ImageView>(R.id.btn_back)
         notificationStartMenstruationButton = view.findViewById<ImageButton>(R.id.btn_start_of_menstruation_notification)
         notificationEndMenstruationButton = view.findViewById<ImageButton>(R.id.btn_end_of_menstruation_notification)
+
+
+        //---> values for switch buttons <---
+        switchStartMenstruationButton = view.findViewById<Switch>(R.id.switch_start_of_menstruation)
+        switchEndMenstruationButton = view.findViewById<Switch>(R.id.switch_end_of_menstruation)
+        switchOvulationButton = view.findViewById<Switch>(R.id.switch_ovulation)
+        switchOpenFetilnostButton = view.findViewById<Switch>(R.id.switch_open_fetilnost)
+        switchCloseFetilnostButton = view.findViewById<Switch>(R.id.switch_closing_ftilnost)
+
+        //<--------------------------------->
+
+
+
+
+        //---> switching notifications <---
+
+        switchStartMenstruationButton.setOnCheckedChangeListener { _, isChecked ->
+
+            if(isChecked) {
+
+
+                var builder = Notification.Builder(context, "1")
+                    //.setSmallIcon(R.drawable.)
+                    .setContentTitle("Hello!")
+                    .setContentText("It's a notificatiion")
+                    //.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                val notification = builder.build()
+
+
+
+            }
+
+        }
+
+        //<--------------------------------->
+
         notificationsButton.setOnClickListener{view ->
             (activity as MenuActivity).apply {
                 menuPresenter.popBackStack(supportFragmentManager)
